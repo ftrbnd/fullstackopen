d7a587afba3b4cfbe82f8cc4563057d9050ce92c
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-11-23 18:44:42 -0800

    Complete 3.15 - 3.18

--- part3/phonebook/src/index.js
+++ part3/phonebook/src/index.js
@@ -7,9 +7,9 @@ const Person = require('./models/Person');
 
 const app = express();
 
+app.use(express.static('dist'));
 app.use(express.json());
 app.use(cors());
-app.use(express.static('dist'));
 
 // 3.7
 app.use(morgan((tokens, req, res) => {
@@ -27,36 +27,65 @@ app.use(morgan((tokens, req, res) => {
     }
 
     return log.join(' ');
-  }))
+}));
 
 // 3.1
 app.get('/api/persons', (req, res) => {
-    Person.find({}).then(persons => {
-        res.json(persons);
-    });
+    Person.find({})
+        .then(persons => {
+            res.json(persons);
+        }).catch(err => {
+            next(err);
+        });
 });
 
 // 3.2
 app.get('/info', (req, res) => {
     const now = new Date();
 
-    Person.find({}).then(persons => {
-        res.send(`
-            <p>Phonebook has info for ${persons.length} people</p>
-            <p>${now}</p>
-        `);
-    });
+    Person.find({})
+        .then(persons => {
+            res.send(`
+                <p>Phonebook has info for ${persons.length} people</p>
+                <p>${now}</p>
+            `);
+        }).catch(err => {    
+            next(err);
+        });
 });
 
 // 3.3
 app.get('/api/persons/:id', (req, res) => {
-    Person.findById(req.params.id).then(person => {
-        res.json({ person });
-    }).catch(err => {
-        return res.status(404).json({
-            error: 'Person not found'
+    Person.findById(req.params.id)
+        .then(person => {
+            if (person) {
+                res.json({ person });
+            } else {
+                res.status(404).end();
+            }
+        }).catch(err => {
+            next(err);
         });
-    });
+});
+
+// 3.17
+app.put('/api/persons/:id', (req, res) => {
+    const person = req.body;
+
+    console.log('new person: ', person);
+
+    if (!person.name || !person.number) {
+        return res.status(400).json({
+            error: 'Name or number is missing'
+        });
+    }
+
+    Person.findByIdAndUpdate(req.params.id, person, { new: true })
+        .then(updatedPerson => {
+            res.json(updatedPerson);
+        }).catch(err => {
+            next(err);
+        });
 });
 
 // 3.4
@@ -65,9 +94,7 @@ app.delete('/api/persons/:id', (req, res) => {
         .then(result => {
             res.status(204).end();
         }).catch(err => {
-            return res.status(404).json({
-                error: 'Person not found'
-            });
+            next(err);
         });
 });
 
@@ -81,21 +108,36 @@ app.post('/api/persons', (req, res) => {
             error: 'Name or number is missing'
         });
     }
-    // else if (persons.some(p => p.name.toLowerCase() === person.name.toLowerCase())) {
-    //     return res.status(400).json({
-    //         error: 'Name already exists'
-    //     });
-    // }
-
+    
     const newPerson = new Person({
         name: person.name,
         number: person.number,
     });
-
-    newPerson.save().then(savedPerson => {
-        res.json(savedPerson);
-    })
+    
+    newPerson.save()
+        .then(savedPerson => {
+            res.json(savedPerson);
+        })
+        .catch(err => {
+            next(err);
+        });
 });
+
+// Unknown endpoint handler
+app.use((request, response) => {
+  response.status(404).send({ error: 'Unknown endpoint' });
+});
+
+// Error handler
+app.use((error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  } 
+
+  next(error);
+})
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
