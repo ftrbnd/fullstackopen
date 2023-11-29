1326a0ce9a3d57aaf4f900dd038da06dcad1f2af
ftrbnd <giosalas25@gmail.com>
2023-11-29 13:53:06 -0800

    Complete 5.6 + 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,31 @@
+import { useState } from 'react'
+
 const Blog = ({ blog }) => {
-  return (<div>
-    {blog.title} {blog.author ? blog.author.username : 'unknown author'}
-  </div>)
+  const [showBlog, setShowBlog] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author ? blog.author.username : 'unknown author'}
+      <button onClick={() => setShowBlog(prev => !prev)}>
+        {showBlog ? 'Hide' : 'View'}
+      </button>
+      {showBlog &&
+        <div>
+          <div>{blog.url}</div>
+          <div>{blog.likes} <button>Like</button></div>
+          <div>{blog.author ? blog.author.username : 'unknown author'}</div>
+        </div>
+      }
+    </div>
+  )
 }
 
 export default Blog
