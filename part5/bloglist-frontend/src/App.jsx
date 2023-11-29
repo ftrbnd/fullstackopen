3058d3c231be705e42f41f8ed9feb296e593f7d7
ftrbnd <giosalas25@gmail.com>
2023-11-29 13:11:19 -0800

    Complete 5.3 + 5.4

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,14 +1,21 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
-import { getAllBlogs, setToken } from './services/blogs'
+import { createBlog, getAllBlogs, setToken } from './services/blogs'
 import { login } from './services/login'
+import './styles.css'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [user, setUser] = useState(null) // has username, token
+  const [notification, setNotification] = useState('')
+  const [notificationType, setNotificationType] = useState('')
   
-  const [username, setUsername] = useState('') 
-  const [password, setPassword] = useState('') 
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
 
   useEffect(() => {
     async function getBlogs() {
@@ -35,23 +42,69 @@ const App = () => {
       const user = await login({ username, password })
       setToken(user.token)
 
-       window.localStorage.setItem(
+      window.localStorage.setItem(
         'bloglistUser', JSON.stringify(user)
-      ) 
+      )
 
       setUser(user)
       setUsername('')
       setPassword('')
     } catch (exception) {
+      setNotification('Invalid credentials')
+      setNotificationType('error')
       console.error(exception);
+
+      setTimeout(() => {
+        setNotification('')
+      }, 5000)
     }
   }
 
-  const handleLogout = async (event) => {
+  const handleLogout = (event) => {
     event.preventDefault()
 
     window.localStorage.removeItem('bloglistUser')
     setUser(null)
+  }
+
+  const addBlog = async (event) => {
+    event.preventDefault()
+
+    if (!title || !author || !url) {
+      setNotification('Please fill in all fields')
+      setNotificationType('error')
+
+      return setTimeout(() => {
+        setNotification('')
+      }, 5000)
+    }
+
+    try {
+      await createBlog({
+        title,
+        author,
+        url
+      })
+
+      setNotification('Blog created')
+      setNotificationType('success')
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+
+      setTimeout(() => {
+        setNotification('')
+      }, 5000)
+
+    } catch (exception) {
+      setNotification('Invalid blog')
+      setNotificationType('error')
+      console.error(exception)
+
+      setTimeout(() => {
+        setNotification('')
+      }, 5000)
+    }
   }
 
   const loginForm = () => (
@@ -78,9 +131,37 @@ const App = () => {
     </form>      
   )
 
+  const blogForm = () => (
+    <form onSubmit={addBlog}>
+      <label>
+      Title
+      <input
+        value={title}
+        onChange={({ target }) => setTitle(target.value)}
+      />
+      </label>
+      <label>
+      Author
+      <input
+        value={author}
+        onChange={({ target }) => setAuthor(target.value)}
+        />
+      </label>
+      <label>
+      Url
+      <input
+        value={url}
+        onChange={({ target }) => setUrl(target.value)}
+        />
+      </label>
+      <button type="submit">Create</button>
+    </form>  
+  )
+
   return (
     <div>
       <h2>Blogs</h2>
+      {notification && <div className={`${notificationType}`}>{ notification }</div>}
 
       {user
         ? (
@@ -90,6 +171,9 @@ const App = () => {
               <Blog key={blog.id} blog={blog} />
             )}
             <button onClick={handleLogout}>Logout</button>
+
+            <h2>New Blog</h2>
+            {blogForm()}
           </div>
         )
         : loginForm()
