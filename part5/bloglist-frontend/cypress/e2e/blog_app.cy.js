aa1ae889ba677179999ab0d115b041ec88ef747a
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-11-30 21:21:07 -0800

    Complete 5.17

---
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -0,0 +1,11 @@
+describe('Blog app', function() {
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    cy.visit('http://localhost:5173')
+  })
+
+  it('Login form is shown', function() {
+    cy.contains('Blogs') // h2
+    cy.contains('Login') // button
+  })
+})
