40c6ee14189aa0bc6764d505cbb203559cd4a0a2
ftrbnd <giosalas25@gmail.com>
2024-01-25 15:31:12 -0800

    Complete 10.23

--- part10/rate-repository-app/src/components/RepositoryList/index.jsx
+++ part10/rate-repository-app/src/components/RepositoryList/index.jsx
@@ -2,17 +2,99 @@ import { FlatList, View, StyleSheet, Pressable } from 'react-native';
 import RepositoryItem from '../RepositoryItem';
 import useRepositories from '../../hooks/useRepositories';
 import { useNavigate } from 'react-router-native';
+import { Picker } from '@react-native-picker/picker';
+import { useEffect, useState } from 'react';
+import Text from '../Text';
 
 const styles = StyleSheet.create({
 	separator: {
 		height: 10,
 		backgroundColor: 'lightgrey',
 	},
+	sort: {
+		backgroundColor: 'lightgrey',
+		textAlign: 'center',
+		padding: 16,
+	},
 });
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
-export const RepositoryListContainer = ({ repositories, openSingleView }) => {
+const SortSelection = ({ refetch }) => {
+	const [showPicker, setShowPicker] = useState(false);
+	const [selection, setSelection] = useState('CREATED_AT');
+
+	useEffect(() => {
+		switch (selection) {
+			case 'CREATED_AT':
+				refetch({ orderBy: 'CREATED_AT', orderDirection: 'DESC' });
+				break;
+			case 'HIGHEST_RATING_AVERAGE':
+				refetch({ orderBy: 'RATING_AVERAGE', orderDirection: 'DESC' });
+				break;
+			case 'LOWEST_RATING_AVERAGE':
+				refetch({ orderBy: 'RATING_AVERAGE', orderDirection: 'ASC' });
+				break;
+			default:
+				refetch({ orderBy: 'CREATED_AT', orderDirection: 'DESC' });
+		}
+
+		setShowPicker(false);
+	}, [selection]);
+
+	const togglePicker = () => {
+		setShowPicker((prev) => !prev);
+	};
+
+	const currentSorting = () => {
+		switch (selection) {
+			case 'CREATED_AT':
+				return 'Latest repositories';
+			case 'HIGHEST_RATING_AVERAGE':
+				return 'Highest rated repositories';
+			case 'LOWEST_RATING_AVERAGE':
+				return 'Lowest rated repositories';
+			default:
+				return 'Latest repositories';
+		}
+	};
+
+	return (
+		<View>
+			<Pressable onPress={togglePicker}>
+				<Text
+					fontWeight={'bold'}
+					style={styles.sort}>
+					Sort by: {currentSorting()}
+				</Text>
+			</Pressable>
+			{showPicker && (
+				<Picker
+					selectedValue={selection}
+					onValueChange={(itemValue) => setSelection(itemValue)}>
+					<Picker.Item
+						label='Latest repositories'
+						value='CREATED_AT'
+					/>
+					<Picker.Item
+						label='Highest rated repositories'
+						value='HIGHEST_RATING_AVERAGE'
+					/>
+					<Picker.Item
+						label='Lowest rated repositories'
+						value='LOWEST_RATING_AVERAGE'
+					/>
+				</Picker>
+			)}
+		</View>
+	);
+};
+
+export const RepositoryListContainer = ({
+	repositories,
+	refetch,
+	openSingleView,
+}) => {
 	const repositoryNodes = repositories
 		? repositories.edges.map((edge) => edge.node)
 		: [];
@@ -20,6 +102,7 @@ export const RepositoryListContainer = ({ repositories, openSingleView }) => {
 	return (
 		<FlatList
 			data={repositoryNodes}
+			ListHeaderComponent={<SortSelection refetch={refetch} />}
 			ItemSeparatorComponent={ItemSeparator}
 			renderItem={({ item }) => (
 				<Pressable onPress={() => openSingleView(item.id)}>
@@ -31,7 +114,7 @@ export const RepositoryListContainer = ({ repositories, openSingleView }) => {
 };
 
 const RepositoryList = () => {
-	const { repositories } = useRepositories();
+	const { repositories, refetch } = useRepositories();
 	const navigate = useNavigate();
 
 	const openSingleView = (id) => {
@@ -41,6 +124,7 @@ const RepositoryList = () => {
 	return (
 		<RepositoryListContainer
 			repositories={repositories}
+			refetch={refetch}
 			openSingleView={openSingleView}
 		/>
 	);
--- part10/rate-repository-app/src/hooks/useRepositories.js
+++ part10/rate-repository-app/src/hooks/useRepositories.js
@@ -1,9 +1,13 @@
 import { useQuery } from '@apollo/client';
 import { GET_REPOSITORIES } from '../graphql/queries';
 
-const useRepositories = () => {
+const useRepositories = (orderBy = 'CREATED_AT', orderDirection = 'DESC') => {
 	const { data, loading, refetch } = useQuery(GET_REPOSITORIES, {
 		fetchPolicy: 'cache-and-network',
+		variables: {
+			orderBy,
+			orderDirection,
+		},
 	});
 
 	return {
