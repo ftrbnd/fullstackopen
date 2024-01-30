4ed1a797083086b2885909aed0b3dbc2eb5a8444
ftrbnd <giosalas25@gmail.com>
2024-01-29 18:04:51 -0800

    Complete 11.9

---
+++ part11/full-stack-open-pokedex/cypress/e2e/pokedex.cy.js
@@ -0,0 +1,17 @@
+describe('Pokedex', function () {
+  beforeEach(function () {
+    cy.visit('http://localhost:5000')
+  })
+
+  it('front page can be opened', function () {
+    cy.contains(/ivysaur/i)
+    cy.contains(
+      'Pokémon and Pokémon character names are trademarks of Nintendo.'
+    )
+  })
+
+  it('pokemon page can be navigated to', function () {
+    cy.contains(/ivysaur/i).click()
+    cy.contains(/chlorophyll/i)
+  })
+})
