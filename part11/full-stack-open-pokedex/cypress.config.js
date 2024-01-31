6364e5302715188f876b30715c426d19db3f15c8
ftrbnd <giosalas25@gmail.com>
2024-01-31 13:04:58 -0800

    Fix lint issue

--- part11/full-stack-open-pokedex/cypress.config.js
+++ part11/full-stack-open-pokedex/cypress.config.js
@@ -3,11 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     // eslint-disable-next-line no-unused-vars
-<<<<<<< HEAD
     setupNodeEvents(on, config) {
-=======
-    setupNodeEvents(_on, _config) {
->>>>>>> 86679f3afae25792c2f48e28404b9bb045e5d54e
       // implement node event listeners here
     },
   },
